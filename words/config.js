e2afa7d2f7a229fa1e065e1aaf4ab15db5423c29
Evan Xu <27160136+ex-git@users.noreply.github.com>
2019-01-28 16:45:06 -0800

    updated API

--- words/config.js
+++ words/config.js
@@ -1,5 +1,5 @@
 'use strict'
 
 //API key
-exports.API_KEY = process.env.API_KEY || '75960a8c-cec5-472d-b698-4af37a196ca3'
-exports.ENDPOINT = process.env.ENDPOINT || 'https://dictionaryapi.com/api/v3/references/sd2/json/'
+exports.API_KEY = process.env.API_KEY
+exports.ENDPOINT = process.env.ENDPOINT
